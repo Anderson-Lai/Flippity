74e6dfca0d7e4e05c7f913a79bdf988ce64183be
Anderson-Lai <andersonmhlai26@gmail.com>
2024-09-23 13:13:22 -0400

    styling

--- src/draw.cpp
+++ src/draw.cpp
@@ -11,11 +11,11 @@ void MainFrame::DrawMainPanel()
             wxTE_READONLY | wxTE_MULTILINE | wxTE_CENTER);
     this->m_timeElapsed = new wxTextCtrl(panel, wxID_ANY, "Time Elapsed:\n", wxDefaultPosition, wxSize(125, 75),
             wxTE_READONLY | wxTE_MULTILINE | wxTE_CENTER);
-    this->m_textCtrl = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(450, 350),
+    this->m_textCtrl = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(510, 425),
            wxTE_READONLY | wxTE_MULTILINE | wxTE_CENTER);
     
     utilsSizer->Add(this->m_flipsIn, wxSizerFlags().Border(wxALL, 25));
-    utilsSizer->Add(this->m_timeElapsed, wxSizerFlags().Border(wxALL, 25));
+    utilsSizer->Add(this->m_timeElapsed, wxSizerFlags().Border(wxLEFT | wxRIGHT | wxBOTTOM, 25));
     sizer->Add(utilsSizer);
     sizer->Add(this->m_textCtrl, wxSizerFlags().Border(wxALL, 25));
 
